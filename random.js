efbdcf928574b24a695a79f03300b7cde57fb3b9
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-05-05 17:34:40 +0600

    random js:

---
+++ random.js
@@ -0,0 +1,12 @@
+var num1 = 2; // integer
+var num2 = 2.6465; // float
+var res1 = Math.floor(num2);
+var res2 = Math.ceil(num2);
+console.log(res1);
+console.log(res2);
+
+
+for (var i = 0; i <= 100; i++) {
+    var x = Math.floor(Math.random() * 6)+1;
+    console.log(x);
+}
