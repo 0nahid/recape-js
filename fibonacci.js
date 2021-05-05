cca742b9b81e23e93f699879cc0cdd0cda7c69ee
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-05-05 17:35:19 +0600

    fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,14 +1,14 @@
-// var fibo = [0, 1];
-// // fibonacci[2] = fibonacci[2 - 1] + fibonacci[2 - 2]
-// // fibonacci[n] = fibonacci[n - 1] + fibonacci[n - 2]
-// function fibonacci(n) {
-//     for (var i = 2; i <= n; i++) {
-//         fibo[i] = fibo[i - 1] + fibo[i - 2];
-//     }
-//     return fibo;
-// }
-// var result = fibonacci(12);
-// console.log(result);
+var fibo = [0, 1];
+// fibonacci[2] = fibonacci[2 - 1] + fibonacci[2 - 2]
+// fibonacci[n] = fibonacci[n - 1] + fibonacci[n - 2]
+function fibonacci(n) {
+    for (var i = 2; i <= n; i++) {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+    return fibo;
+}
+var result = fibonacci(12);
+console.log(result);
 
 var fibo = [0, 1];
 
